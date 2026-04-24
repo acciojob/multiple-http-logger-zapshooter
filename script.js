b62b472c9b451f08ca8eb2a521f5544476bc4431
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-04-24 23:44:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const urls = [
   "https://jsonplaceholder.typicode.com/posts/2",
   "https://jsonplaceholder.typicode.com/posts/3",
 ];
-
 // Don't change the above line
 // Write your code here
+
+const fetchAll = async () => {
+  const promises = urls.map(url => fetch(url).then(res => res.json()));
+  const responses = await Promise.all(promises);
+  responses.forEach(data => console.log(data));
+};
+
+fetchAll();
